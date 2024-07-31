c6dfc19f582ddc71dbd9ca5e85124f456a69db8e
fujisawa <rush2065kingcrab0120@gmail.com>
2024-08-01 01:25:12 +0900

    Add:Map layout

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get    "/contact", to: "static_pages#contact"
   get    "/signup",  to: "users#new"
   get    "/login",   to: "sessions#new"
+  get    "search",  to: "static_pages#search"
   post   "/login",   to: "sessions#create"
   delete "/logout",  to: "sessions#destroy"
   resources :users
