626ade61512a44e15cb7d25681f07bfff43b871e
fujisawa <rush2065kingcrab0120@gmail.com>
2024-08-13 20:12:36 +0900

    fix:spot_test

--- test/models/spot_test.rb
+++ test/models/spot_test.rb
@@ -2,9 +2,10 @@ require "test_helper"
 
 class SpotTest < ActiveSupport::TestCase
   def setup
+    @prefecture = Prefecture.create!(name: "沖縄県")
     @spot = Spot.new(name: "オールブルー", address: "沖縄県竹富島1-2-3",
                      description: "正解中の海の生き物がいるダイビングスポットです",
-                     image: "dummy_image.jpg")
+                     image: "dummy_image.jpg", prefecture: @prefecture)
   end
 
   test "should be valid" do
