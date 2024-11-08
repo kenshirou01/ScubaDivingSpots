d7477fc943d7bebb138491774e460cd48ba70c30
fujisawa <rush2065kingcrab0120@gmail.com>
2024-11-08 18:53:50 +0900

    fix: layout

--- app/controllers/prefectures_controller.rb
+++ app/controllers/prefectures_controller.rb
@@ -1,5 +1,4 @@
 class PrefecturesController < ApplicationController
-  # Get /prefectures/:id
   def show
     @prefecture = Prefecture.find(params[:id])
     @spots = @prefecture.spots
--- test/controllers/creatures_controller_test.rb
+++ test/controllers/creatures_controller_test.rb
@@ -1,14 +1,14 @@
-require "test_helper"
+require 'test_helper'
 
 class CreaturesControllerTest < ActionDispatch::IntegrationTest
-  def setup
-    @creature = creatures(:one) # 事前に fixtures または factories でデータをセットアップしておく
+  setup do
+    @creature = creatures(:one)
   end
 
   test "should get index" do
     get creatures_url
     assert_response :success
-    assert_select "title", "Creatures | #{Rails.application.config.app_name}"
+    assert_not_nil assigns(:creatures)
   end
 
   test "should get new" do
@@ -17,11 +17,17 @@ class CreaturesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should create creature" do
-    assert_difference('Creature.count', 1) do
-      post creatures_url, params: { creature: { name: 'New Creature', description: 'A new creature', periods: 'All year', photo: fixture_file_upload('test/fixtures/files/sample.jpg', 'image/jpeg') } }
+    assert_difference('Creature.count') do
+      post creatures_url, params: { creature: { name: 'MyString', description: 'MyText', periods: 'MyText', photo: nil } }
     end
-
     assert_redirected_to creature_url(Creature.last)
+  end
+
+  test "should not create invalid creature" do
+    assert_no_difference('Creature.count') do
+      post creatures_url, params: { creature: { name: '', description: '', periods: '' } }
+    end
+    assert_response :unprocessable_entity
   end
 
   test "should show creature" do
@@ -35,19 +41,25 @@ class CreaturesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should update creature" do
-    patch creature_url(@creature), params: { creature: { name: 'Updated Name', description: 'Updated description', periods: 'Seasonal', photo: fixture_file_upload('test/fixtures/files/sample.jpg', 'image/jpeg') } }
+    patch creature_url(@creature), params: { creature: { name: 'Updated Creature', description: 'Updated description', periods: 'July-September' } }
     assert_redirected_to creature_url(@creature)
-    @creature.reload
-    assert_equal 'Updated Name', @creature.name
-    assert_equal 'Updated description', @creature.description
-    assert_equal 'Seasonal', @creature.periods
+  end
+
+  test "should not update invalid creature" do
+    patch creature_url(@creature), params: { creature: { name: '', description: '', periods: '' } }
+    assert_response :unprocessable_entity
   end
 
   test "should destroy creature" do
     assert_difference('Creature.count', -1) do
       delete creature_url(@creature)
     end
-
     assert_redirected_to creatures_url
+  end
+
+  test "should search creatures" do
+    get creature_search_url(query: 'shark')
+    assert_response :success
+    assert_not_nil assigns(:creatures)
   end
 end
